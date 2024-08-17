049ce2992dd0cf5a377bcb730fb7463956f4e545
Seth Strouf <seth.strouf@doane.edu>
2024-08-17 14:03:41 -0600

    Complete 2.2 and 2.3

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import Part from './Part'
 
 const Content = ({ parts }) => {
+  const sumOfExercises = () => {
+    return parts.reduce((acc, curr) => acc + curr.exercises, 0)
+  }
+
   return (
     <div>
       {parts.map(part =>
         <Part key={part.id} part={part} />
       )}
+      <strong>Total of {sumOfExercises()} exercises</strong>
     </div>
   )
 }
