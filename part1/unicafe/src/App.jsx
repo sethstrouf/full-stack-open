8f91c0631b8eb0cdb7f0cc9bc47ad334a367a3ba
Seth Strouf <seth.strouf@doane.edu>
2024-08-17 12:09:09 -0600

    Complete 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,18 +8,37 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allCounts, setAllCounts] = useState(0)
+
+  const handleGoodFeedback = () => {
+    setGood(good + 1)
+    setAllCounts(allCounts + 1)
+  }
+
+  const handleNeutralFeedback = () => {
+    setNeutral(neutral + 1)
+    setAllCounts(allCounts + 1)
+  }
+
+  const handleBadFeedback = () => {
+    setBad(bad + 1)
+    setAllCounts(allCounts + 1)
+  }
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text="Good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      <Button handleClick={handleGoodFeedback} text="Good" />
+      <Button handleClick={handleNeutralFeedback} text="Neutral" />
+      <Button handleClick={handleBadFeedback} text="Bad" />
 
       <h1>Statistics</h1>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {allCounts}</p>
+      <p>Average {(good + -bad) / allCounts}</p>
+      <p>Positive {good / allCounts * 100} %</p>
     </div>
   )
 }
