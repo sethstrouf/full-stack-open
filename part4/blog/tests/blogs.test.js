30422a40c1dd461a874ced673b0945f64bfcda4f
Seth Strouf <seth.strouf@doane.edu>
2024-08-23 12:56:28 -0600

    Complete 4.4

--- part4/blog/tests/blogs.test.js
+++ part4/blog/tests/blogs.test.js
@@ -2,9 +2,56 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
+test('dummy returns 1', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithTwoBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'This is the Second Blog',
+      author: 'Gruber Adams',
+      url: 'https://example.com',
+      likes: 15,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated correctly', () => {
+    const result = listHelper.totalLikes(listWithTwoBlogs)
+    assert.strictEqual(result, 20)
+  })
 })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length > 0
+    ? blogs.map(blog => blog.likes).reduce((sum, likes) => {
+      return sum + likes
+    })
+    : 0
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
