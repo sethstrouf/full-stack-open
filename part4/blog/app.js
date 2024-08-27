db3146b8409fcd9b1a893a36ee7d54e21b7b20ad
Seth Strouf <seth.strouf@doane.edu>
2024-08-27 10:57:15 -0600

    Complete 4.15

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -2,6 +2,7 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -21,6 +22,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
